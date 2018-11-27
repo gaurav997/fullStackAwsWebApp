bd5716524091f5cf209a1e61c45b190d032076a4
Gaurav Gupta <gupta.d.gaurav@gmail.com>
2018-11-27 18:31:35 +0530

    Created project skeleton and commited to remote

---
+++ src/main/java/com/myProjects/fullStackAwsWebApp/FullStackAwsWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.myProjects.fullStackAwsWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackAwsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackAwsWebAppApplication.class, args);
+	}
+}
